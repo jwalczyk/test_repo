73ff64ca5afc8653e41c2237bee735973a428dac
John Walczyk <jwalczyk@us.ibm.com>
2017-09-20 16:36:20 -0400

    make slight change to python_read_5.py

--- example/python_read_5.py
+++ example/python_read_5.py
@@ -1,7 +1,7 @@
 # from sys module import a member called 'argv'
 from sys import argv
 #unpack
-# test 4 - test when a commit closes the issue; yadda
+# test 5 - test PR title
 script, filename = argv
 fp = open(filename)
 print "Reading file %r " % fp
