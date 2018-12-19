e9c7752b9a86af53520176e74d762f0ea582fc97
Bizzaro World John <jhwalczyk12@gmail.com>
2018-12-19 14:17:07 -0500

    Bizzaro John strikes again

--- example/python_read.py
+++ example/python_read.py
@@ -1,14 +1,1 @@
-# from sys module import a member called 'argv'
-from sys import argv
-#unpack
-# test 3
-script, filename = argv
-fp = open(filename)
-print "Reading file %r " % fp
-print fp.read()
-fp.close()
-
-# gui stream test
-# gui test for seq no
-# yet another stream test 
-# another stream test 
+# Bizzaro John hates this file
